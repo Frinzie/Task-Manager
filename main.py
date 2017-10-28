709f0ab58726624c2ba3b454800c12063250c2c0
frinzie <janlasekpena01@gmail.com>
2017-10-28 13:31:26 +0200

    prowizoryczny parser dodany

---
+++ main.py
@@ -0,0 +1,13 @@
+# import kivy
+# from kivy.app import App
+# from kivy.uix.label import Label
+# kivy.require("1.10.0")
+
+
+# class MyApp(App):
+#     def build(self):
+#         return Label(text="Hello World")
+
+
+# if __name__ == "__main__":
+#     MyApp().run()
---
+++ parser.py
@@ -0,0 +1,22 @@
+doc_string = """Module that provides a parser function which takes a string
+and returns a dictionary with the note, tags, date, reminders and errors"""
+
+
+def parser(text: str):
+    note = ""
+    tags = []
+    for word in text.split():
+        if word[0] == "#":
+            tags.append(word[1:])
+            continue
+        note += word + " "
+    return {"note": note.strip(), "tags": tags}
+
+
+if __name__ == "__main__":
+    print(doc_string)
+    inp = None
+    while 1:
+        inp = input()
+        if inp == "quit()": break
+        print(parser(inp))
